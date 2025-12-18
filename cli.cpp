e8fdc4b225ea4d6a4b9a58fb624af8d8d4bc8478
lifegpc <root@lifegpc.com>
2025-12-18 16:25:38 +0800

    Add a speedtest command to cli

--- cli.cpp
+++ cli.cpp
@@ -6,6 +6,7 @@
 #include "fileop.h"
 #include <unordered_map>
 #include <inttypes.h>
+#include "time_util.h"
 
 int main(int argc, char* argv[]) {
 #if _WIN32
@@ -24,7 +25,9 @@ int main(int argc, char* argv[]) {
     }
 #endif
     if (args.size() < 3) {
-        printf("Usage: %s ls/extract <xp3 file>\n", args[0].c_str());
+        printf("Usage: %s extract <xp3 file> Extract files\n", args[0].c_str());
+        printf("       %s ls <xp3 file> List files in the archive\n", args[0].c_str());
+        printf("       %s speedtest <xp3 file> Test extraction speed (no files will be written)\n", args[0].c_str());
         return 1;
     }
     std::string action = args[1];
@@ -89,6 +92,36 @@ int main(int argc, char* argv[]) {
             }
             delete inf;
         }
+    } else if (action == "speedtest") {
+        const size_t chunk_size = 8192;
+        auto start_time = time_util::time_ns64();
+        uint8_t buffer[chunk_size];
+        Xp3Archive archive(xp3file.c_str());
+        if (!archive.ReadIndex()) {
+            printf("Failed to read index from %s\n", xp3file.c_str());
+            return 1;
+        }
+        uint64_t total_size = 0;
+        for (const auto& file: archive.files) {
+            Xp3File* inf = archive.OpenFile(file);
+            if (!inf) {
+                printf("Failed to open file %s\n", file.filename.c_str());
+                continue;
+            }
+            uint64_t total_read = 0;
+            while (true) {
+                size_t r = inf->read(buffer, chunk_size);
+                if (r == 0) break;
+                total_read += r;
+            }
+            total_size += total_read;
+            delete inf;
+        }
+        auto end_time = time_util::time_ns();
+        double elapsed_sec = (end_time - start_time) / 1e9;
+        double speed = total_size / elapsed_sec / (1024 * 1024);
+        printf("Extracted %" PRIu64 " bytes in %.6f seconds (%.2f MB/s)\n", total_size, elapsed_sec, speed);
+        return 0;
     } else {
         printf("Unknown action: %s\n", action.c_str());
         return 1;
